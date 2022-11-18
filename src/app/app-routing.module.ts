52041f670250a2f30f0022b08a2ef35971ed9690
LeoKGX <56890052+LeoKGX@users.noreply.github.com>
2022-11-18 01:21:48 -0300

    Muevo el Boton cerrar sesion

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,7 +7,8 @@ import { PortfolioComponent } from './components/portfolio/portfolio.component';
 const routes: Routes = [
   {path: "portfolio" , component :PortfolioComponent}, //canActivate: [GuardGuard]},
   {path: "login", component: LoginComponent},
-  {path: "", redirectTo: "portfolio", pathMatch: "full"}
+  {path: "", redirectTo: "portfolio", pathMatch: "full"},
+  {path: "/", redirectTo: "portfolio", pathMatch: "full"}
 ];
 
 @NgModule({
